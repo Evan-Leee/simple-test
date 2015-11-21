4c22073667ba8eb036c1bd1c40dfa1f0145eefa2
Evan-Leee <EvanLee_@outlook.com>
2015-11-22 00:22:01 +0800

    finish the second part

--- src/calculate-second.js
+++ src/calculate-second.js
@@ -1,3 +1,17 @@
 function calculate_median(arr) {
+    var result = [];
+    var temp = [];
 
+    temp = arr.filter(function (item, i) {
+        return (i + 1) % 2 === 0;
+    });
+
+    var index;
+    if (temp.length % 2 === 0) {
+        index = temp.length / 2 - 1;
+        return parseInt((temp[index] + temp[index + 1]) / 2);
+    } else {
+        index = Math.floor(temp.length / 2);
+        return temp[index];
+    }
 }
