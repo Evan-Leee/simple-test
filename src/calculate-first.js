231cbfbff169b3fbe49338aa4d679a8bf6044ede
Evan-Leee <EvanLee_@outlook.com>
2015-11-21 22:23:20 +0800

    fail to finish at least one part

--- src/calculate-first.js
+++ src/calculate-first.js
@@ -1,3 +1,14 @@
 function single_element(arr) {
-  
+    var result = [];
+
+    result = arr.filter(function(item,i){
+        if((i+1)%2 === 0){
+            return item;
+        }
+    });
+
+    for(var i = 0; i < result.length; i++)
+
+
+    return result;
 }
